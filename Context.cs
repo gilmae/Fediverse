b8c6e76aacc911adfb7fccf50d599e3c614a0754
gilmae <avocadia@fastmail.fm>
2025-01-10 17:34:57 +1100

    Supports retrieving full objects.
    Including requesting them from the id

--- Context.cs
+++ Context.cs
@@ -6,10 +6,12 @@ namespace Fediverse;
 public class Context {
     private LinkGenerator _linkGenerator;
     private IServiceProvider _serviceProvider;
+    private HttpClient _httpClient;
 
     public Context(IServiceProvider serviceProvider, LinkGenerator linkGenerator) {
         _serviceProvider = serviceProvider;
         _linkGenerator = linkGenerator;
+        _httpClient = httpClientFactory.CreateClient("activityPub");
     }
 
     public string? GetActorUri(string identifier) 
@@ -39,5 +41,34 @@ public class Context {
         }
 
         return _linkGenerator.GetUriByRouteValues(httpContextAccessor.HttpContext, "inboxEndpoint", new { identifier });
+    }
+    public async Task<T?> GetObject<T>(AS.IObjectOrLink o)
+    {
+        T? obj = o switch
+        {
+            T => (T)o,
+            AS.Object => (T)o,
+            AS.Link => await GetObject<T>((o as AS.Link)?.Href?.ToString() ?? ""),
+            _ => default
+        };
+        return obj;
+
+    }
+
+    private async Task<T?> GetObject<T>(string iri)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, iri);
+        request.Headers.Add("Accept", "application/activity+json");
+        using HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+        response.EnsureSuccessStatusCode();
+
+        string body = await response.Content.ReadAsStringAsync();
+        if (string.IsNullOrEmpty(body))
+        {
+            return default;
+        }
+
+        return JsonSerializer.Deserialize<T>(body);
     }
 }
