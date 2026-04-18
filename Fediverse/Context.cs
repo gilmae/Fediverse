75f7f6c3dd141d71c274991450c629b44718dec8
gilmae <avocadia@fastmail.fm>
2026-04-18 17:41:49 +1000

    Allow GetObject by iri

--- Fediverse/Context.cs
+++ Fediverse/Context.cs
@@ -24,7 +24,7 @@ public class Context
         _activityPub = activityPub;
         _logger = logger;
     }
-    public Uri Url() 
+    public Uri Url()
     {
         var httpContextAccessor = _serviceProvider.GetService(typeof(IHttpContextAccessor)) as IHttpContextAccessor;
         if (httpContextAccessor == null)
@@ -48,10 +48,11 @@ public class Context
         using HttpContent body = new StringContent(serialisedData, encoding: Encoding.UTF8, mediaType: "application/activity+json");
 
         string? endpoint = (await GetObject<Actor>(recipient))?.Inbox?.Id;
-        if (endpoint == null) {
+        if (endpoint == null)
+        {
             return; //false
         }
-            
+
         var request = new HttpRequestMessage(HttpMethod.Post, endpoint)
         {
             Content = body,
@@ -64,7 +65,7 @@ public class Context
         // }
 
         using HttpResponseMessage response = await _httpClient.SendAsync(request);
-        var responseStream  = await response.Content.ReadAsStreamAsync();
+        var responseStream = await response.Content.ReadAsStreamAsync();
         using StreamReader reader = new StreamReader(responseStream);
         var responseMessage = reader.ReadToEnd();
 
@@ -81,7 +82,7 @@ public class Context
     {
         return GetLink(RoutingNames.Profile, new { identifier }).ToString();
     }
-    
+
     public string? GetThingUri(string user, string identifier)
     {
         return GetLink(RoutingNames.Thing, new { user, identifier }).ToString();
@@ -97,23 +98,27 @@ public class Context
         return GetLink(RoutingNames.Followers, new { identifier }).ToString();
     }
 
-    public string? GetFollowingUri(string identifier) {
-      return GetLink(RoutingNames.Following, new { identifier }).ToString(); 
+    public string? GetFollowingUri(string identifier)
+    {
+        return GetLink(RoutingNames.Following, new { identifier }).ToString();
     }
 
-    public string? GetInboxUri(string identifier) {
-      return GetLink(RoutingNames.Inbox, new { identifier }).ToString(); 
+    public string? GetInboxUri(string identifier)
+    {
+        return GetLink(RoutingNames.Inbox, new { identifier }).ToString();
     }
 
-    public string? GetOutboxUri(string identifier) {
-      return GetLink(RoutingNames.Outbox, new { identifier }).ToString(); 
+    public string? GetOutboxUri(string identifier)
+    {
+        return GetLink(RoutingNames.Outbox, new { identifier }).ToString();
     }
 
-    public async Task<T?> GetObject<T>(AS.IObjectOrLink o)
+    public async Task<T?> GetObject<T>(object o)
     {
         T? obj = o switch
         {
             T => (T)o,
+            string => await GetObject<T>(o),
             AS.Object => (T)o,
             AS.Link => await GetObject<T>((o as AS.Link)?.Href?.ToString() ?? ""),
             _ => default
@@ -139,19 +144,22 @@ public class Context
         return JsonSerializer.Deserialize<T>(body);
     }
 
-    private Uri GetLink(string routeName, object? routeValues) {
+    private Uri GetLink(string routeName, object? routeValues)
+    {
         LinkGenerator? linkGenerator = _serviceProvider.GetService(typeof(LinkGenerator)) as LinkGenerator;
-        if (linkGenerator == null) {
+        if (linkGenerator == null)
+        {
             throw new ArgumentNullException(nameof(linkGenerator));
         }
 
         string? uri = linkGenerator.GetUriByRouteValues(routeName, routeValues, "https", new HostString(_activityPub.GetHost()));
 
-        if (string.IsNullOrEmpty(uri)) {
+        if (string.IsNullOrEmpty(uri))
+        {
             throw new ArgumentException(nameof(uri));
         }
 
-        return new Uri(uri);        
+        return new Uri(uri);
     }
 
 }
